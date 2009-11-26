4b2303fd3f7936ff65f4edbd68edb429dacf457e
Adrian Bowyer <A.Bowyer@bath.ac.uk>
2009-11-26 13:01:04 +0000

    Tidying and adding extra comments to the configuration.h files.

--- firmware/FiveD_GCode/Extruder/extruder.h
+++ firmware/FiveD_GCode/Extruder/extruder.h
@@ -23,6 +23,68 @@
 #define TEMP_PID_IGAIN 0.1
 #define TEMP_PID_DGAIN 100.0
 
+//******************************************************************************************************
+
+// Divide by this to correct for the fact that we have
+// messed up the timer clock
+
+#define MILLI_CORRECTION 64
+
+// The temperature routines get called each time the main loop
+// has gone round this many times
+
+#define SLOW_CLOCK 5000
+
+// Default PWM for the extruder stepper
+
+#define STEP_PWM 150
+
+// Pin defintion section.  This is for the RepRap Extruder Controler V2.2
+
+//our RS485 pins
+
+#define RX_ENABLE_PIN 4 
+#define TX_ENABLE_PIN 16  
+
+// Pins to direct-drive the extruder stepper
+
+#define E_STEP_PIN 10
+#define E_DIR_PIN 9 
+
+#ifdef MAX6675_THERMOCOUPLE
+// I2C pins for the MAX 6675 temperature chip
+ #define SO 18    // MISO
+ #define SCK 19   // Serial Clock
+ #define TC_0 17  // CS Pin of MAX6607
+#else
+ #define TEMP_PIN 3
+#endif
+
+// Control pins for the A3949 chips
+
+#define H1D 7
+#define H1E 5
+#define H2D 8
+#define H2E 6
+
+// Analogue read of this pin gets the potentiometer setting
+
+#define POT 0
+
+// MOSFET drivers
+
+#define OUTPUT_A 15
+#define OUTPUT_B 11
+#define OUTPUT_C 12
+
+#define DEBUG_PIN 13
+
+// The LED blink function
+
+extern void blink(bool on);
+
+// ******************************************************************************
+
 class extruder
 {
 
